bb943b7aab1b102d6d173539d22bd9270a0e0590
jihyunle <jennifer.jlee@outlook.com>
2019-02-05 17:29:55 -0500

    Produces correct result but need to come back to inner loop

--- src/arraysLockerProblem.java
+++ src/arraysLockerProblem.java
@@ -6,122 +6,46 @@ Then, starting with the third locker, for this locker and every third locker aft
 Continue with the nth locker changing the “position” of every nth locker until n = 100.
 Print out the locker numbers that remain open. What do you notice about the lockers that remain open?*/
 
-
-
 public class arraysLockerProblem {
-    
+
     public static void main(String[]args){
         boolean[] lockers = new boolean[100];
-        /*default is false
-        false = open
-        true = closed*/
 
+        // Open all lockers by setting bool value to true
+//        openAllLockers(lockers);
+
+        // Change locker status as needed
+        changeLockerStatus(lockers);
+
+        // Print
+        print(lockers);
+    }
+
+    public static void openAllLockers(boolean[] lockers){
         for (int i=0; i<lockers.length; i++){
-            /*First, open all lockers
-             * open = true*/
-//            lockers[i]=true;
-            int lockerNum = i+1;
+            lockers[i] = true;
+        }
+    }
 
-            /*if divisible by 2, close*/
-            if (lockerNum%2==0){
-                lockers[i]=true;
+    public static void changeLockerStatus(boolean[] lockers){
+        /*
+        * Change the status of nth locker as n approaches 100
+        * Outer loop goes through every locker*/
+        for (int i = 0; i < lockers.length; i++){
+
+            // Inner loop does the closing/opening every nth locker
+            for (int j = i; j < lockers.length; j += (i+1)){
+                    lockers[j] = !lockers[j];
             }
+        }
+    }
 
-            /*if divisible by 3
-             * open or close as appropriate*/
-            if (lockerNum%3==0){
-                if (lockers[i]=true){
-                    lockers[i]=false;
-                } else {
-                    lockers[i]=true;
-                }
-            }
-
-            if (lockers[i]=false){
+    public static void print(boolean[] lockers){
+        for (int i = 0; i < lockers.length; i++){
+            if (lockers[i]){
                 System.out.print(i+1 + "  ");
             }
         }
-
-        for (int i=0; i<lockers.length; i++){
-            if (lockers[i]=false){
-                System.out.print(i+1 + "  ");
-            }
-        }
-
-//        for (int i=0; i<lockers.length; i++){
-
-//            /*if locker is open set boolean to true
-//            * if divisible by 2, close or set boolean to false*/
-//            if ((i+1)%2==0){
-//                lockers[i]=false;
-//            }
-
-//            /*if divisible by 3
-//            * open or close as appropriate*/
-//            if ((i+1)%3==0){
-//                if (lockers[i]=false){
-//                    lockers[i]=true;
-//                } else {
-//                    lockers[i]=true;
-//                }
-//            }
-//        }
-
-
     }
 
-//    public static void main(String[]args){
-//
-//        // Assign numbers 1~100 to lockers
-//        int[] lockers = new int[100];
-//        for (int i=0; i<lockers.length; i++){
-//            lockers[i] = i+1;
-//        }
-//
-//        // Start with all lockers open
-////        boolean isOpen;
-//        int[] openLockers = new int[100];
-//
-//        for (int n:lockers){
-//            isOpen = true;
-//            if (n%2==0){
-//                isOpen = false;
-//                lockerStatus(n, openLockers);
-//            }
-//        }
-//
-//        for (int n:lockers){
-//            if (n%3==0){
-//
-//            }
-//        }
-//
-//        for (int n:lockers){
-//            if (isOpen=true){
-//
-//            }
-//        }
-//
-//    }
-
-    public static boolean openOrClose(boolean oc){
-        return !oc;
-    }
-
-
-//    public static void lockerStatus(int n, int[]ar){
-//        for (int i=0; i<100; i++){
-//            ar[i] = n;
-//        }
-//
-//        int numOpened = 100;
-//        int numClosed = 0;
-//        if (isOpen==true){
-//            numOpened--;
-//
-//        } else if (isOpen==false){
-//            numClosed++;
-//        }
-//
-//    }
 }
